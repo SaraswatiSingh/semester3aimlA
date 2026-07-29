e57b269d8d89aea1a5807e6ca6a9a717b09602c9
unknown <saurwisingh06@gmail.com>
2026-07-29 14:50:24 +0530

    initial commit

---
+++ Day1/index.js
@@ -0,0 +1,27 @@
+const app =require('./app.js');
+
+// let a=4;
+// let b=3;
+// let c=5;
+// console.log(a+b+c);
+// console.log("saraswati");
+//  var x=20;
+//  if(x==40){
+//     console.log("x is 20");
+//  }
+//  else{
+//     console.log("x is not 20");
+//  }
+//  for (var i=0; i<5; i++){
+//     console.log(i)
+//  }
+
+//  var x=20;
+//  const arr=[1,2,3,4,5];
+//  for (var i=0; i<arr.length; i++){
+//     // console.log(arr[i]);
+//     // console.log(arr[2]);
+//  }
+//  console.log(arr[3]);
+
+ console.log(app.xz);
